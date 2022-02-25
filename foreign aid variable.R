7fba28608227451bd1e1981451c82cf74c811d39
Shari Tian <sharitian2000@gmail.com>
2022-02-25 02:05:30 -0500

    foreign_aid variable

---
+++ foreign aid variable.R
@@ -0,0 +1,31 @@
+library(tidyverse)
+library(sf)
+
+foreign_aid <- read_csv("foreign_aid_data.csv")
+
+# Zimbabwe
+
+zim <- readRDS("DHS_data/Zimbabwe/Zimbabwe_final.RDS")
+
+zim_foreign <- foreign_aid %>%
+  filter(Country == "Zimbabwe") %>% 
+  select(-Country) %>%
+  right_join(zim, by = c("Year" = "year")) %>%
+  rename(foreign_aid = Value)
+
+saveRDS(zim_foreign, file=paste0("DHS_data/Zimbabwe/Zimbabwe_final.RDS"))
+
+# Mali
+
+mal <- readRDS("DHS_data/Mali/Mali_final.RDS")
+
+mal_foreign <- foreign_aid %>%
+  filter(Country == "Mali") %>% 
+  select(-Country) %>%
+  right_join(mal, by = c("Year" = "year")) %>%
+  rename(foreign_aid = Value)
+
+saveRDS(mal_foreign, file=paste0("DHS_data/Mali/Mali_final.RDS"))
+
+# Nigeria
+
